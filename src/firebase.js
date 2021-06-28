a6c2d5b597e50c93964c597ac0ebcff874f186b6
Dev <devaduthan.n@beinex.com>
2021-06-28 23:59:44 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ9sVR7o0j4TbEh6GmJoecCfoKVz2KELI",
+    authDomain: "fb-clone-9d1eb.firebaseapp.com",
+    projectId: "fb-clone-9d1eb",
+    storageBucket: "fb-clone-9d1eb.appspot.com",
+    messagingSenderId: "253111478152",
+    appId: "1:253111478152:web:c3114ed48001428308bc3d"
+  };
